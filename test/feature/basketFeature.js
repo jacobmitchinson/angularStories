5cd8959c0712d569e5e2c79746f44f16c0140b99
Jacob Mitchinson <jmitchinson+github@gmail.com>
2015-03-30 15:50:54 +0100

    Updated feature to snrue basket could remove items

--- test/feature/basketFeature.js
+++ test/feature/basketFeature.js
@@ -4,10 +4,25 @@ describe('Basket', function() {
     browser.get('/app/index.html');
   });
 
-  it('can add items to the basket', function() { 
+  function addToBasket() { 
     element.all(by.css(".add-to-basket")).first().click();
+  };
+
+  function removeFromBasket() { 
+    element.all(by.css(".remove-from-basket")).first().click();
+  }
+
+  it('can add items to the basket', function() { 
+    addToBasket();
     var el = element.all(by.repeater('item in basket'));
     expect(el.count()).toEqual(1);
   }); 
+
+  it('can remove items from the basket', function() { 
+    addToBasket();
+    removeFromBasket();
+    var el = element.all(by.repeater('item in basket'));
+    expect(el.count()).toEqual(0);
+  });
 
 }); 
