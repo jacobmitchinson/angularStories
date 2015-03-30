8ca97cda2385bf1870c93d17b3a558bed399c610
Jacob Mitchinson <jmitchinson+github@gmail.com>
2015-03-30 20:04:05 +0100

    Updated controller and implemented 10 and 15 poud voucherws

--- app/js/homepageController.js
+++ app/js/homepageController.js
@@ -9,6 +9,7 @@ fashionApp.controller('homepageController', function($scope, $http) {
 
   $scope.basket = [];
   $scope.total = 0;
+  $scope.voucher;
 
   $scope.add = function(item) { 
     $scope.basket.push(item);
@@ -29,7 +30,7 @@ fashionApp.controller('homepageController', function($scope, $http) {
   $scope.applyVoucher = function(voucher) { 
     for(var i = 0; i < $scope.vouchers.length; i++) { 
       if($scope._checkVoucherName($scope.vouchers[i], voucher)) { 
-        if($scope._checkPriceConditions($scope.vouchers[i])) { 
+        if($scope._checkPriceConditions($scope.vouchers[i]) && $scope._checkOtherConditions($scope.vouchers[i])) { 
           $scope.total = $scope.calculateTotal() - $scope.vouchers[i].price;
         };
       }
@@ -43,12 +44,28 @@ fashionApp.controller('homepageController', function($scope, $http) {
   };
 
   $scope._checkPriceConditions = function(voucher) { 
+    console.log(voucher)
     if($scope.calculateTotal() > voucher.totalReq) { 
       return true;
     }
   };
 
-  // $scope._checkOtherConditions = function(voucher) { 
+  $scope._checkOtherConditions = function(voucher) { 
+    if(voucher.orConditions != undefined) { 
+      var check = $scope._findValues($scope.basket);
+      console.log(check);
+    } else {  
+      return true;
+    }
+  }
 
-  // }
+  $scope._findValues = function(obj) { 
+    var keys = Object.keys(obj);
+    var length = keys.length;
+    var values = Array(length);
+    for (var i = 0; i < length; i++) {
+      values[i] = obj[keys[i]];
+    }
+    return values;
+  };
 });
