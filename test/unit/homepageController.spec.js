0fbb8e5c63f1c42385c5c9c9b18f08cc917d77e6
Jacob Mitchinson <jmitchinson+github@gmail.com>
2015-03-30 15:50:32 +0100

    Updated feature to ensure basket could remove items

--- test/unit/homepageController.spec.js
+++ test/unit/homepageController.spec.js
@@ -7,21 +7,34 @@ describe('homepageController', function() {
   beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
     $httpBackend = _$httpBackend_;
     $httpBackend.expectGET('clothes/fullClothesList.json').
-        respond([{name: 'Clothing1'}, {name: 'Clothing2'}]);
+        respond([{name: 'Clothing1', price: 99}, {name: 'Clothing2', price: 10}]);
     scope = $rootScope.$new();
     ctrl = $controller('homepageController', {$scope: scope});
+    $httpBackend.flush();
   }));
 
+  function add() { 
+    scope.add(scope.clothes[0]);
+  };
+
   it('can return the items', function() { 
-    expect(scope.clothes).toBeUndefined();
-    $httpBackend.flush();
-    expect(scope.clothes).toEqual([{name: 'Clothing1'}, {name: 'Clothing2'}]);
+    expect(scope.clothes).toEqual([{name: 'Clothing1', price: 99}, {name: 'Clothing2', price: 10}]);
   });
 
   it('can add items to the basket', function() { 
-    $httpBackend.flush();
-    scope.add(scope.clothes[0]);
+    add();
     expect(scope.basket.length).toEqual(1);
+  });
+
+  it('can remove items from the basket', function() { 
+    add();
+    scope.remove(scope.clothes[0]);
+    expect(scope.basket.length).toEqual(0);
+  });
+
+  it('can total the items in the basket', function() { 
+    add();
+    expect(scope.total()).toEqual(99);
   });
 
 });
