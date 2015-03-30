8301175585caf9a2e990d338e28abdf87a7c6c41
Jacob Mitchinson <jmitchinson+github@gmail.com>
2015-03-30 20:04:54 +0100

    Updated views to reflect changes in voucher strucutre

--- test/feature/basketFeature.js
+++ test/feature/basketFeature.js
@@ -12,17 +12,23 @@ describe('Basket', function() {
     element.all(by.css(".remove-from-basket")).first().click();
   }
 
-  it('can add items to the basket', function() { 
+  it('can add items to the order', function() { 
     addToBasket();
     var el = element.all(by.repeater('item in basket'));
     expect(el.count()).toEqual(1);
   }); 
 
-  it('can remove items from the basket', function() { 
+  it('can remove items from the order', function() { 
     addToBasket();
     removeFromBasket();
     var el = element.all(by.repeater('item in basket'));
     expect(el.count()).toEqual(0);
+  });
+
+  it('can show the total price of the order', function() { 
+    addToBasket();
+    var el = element.all(by.id('total-price')).first();
+    expect(el.getText()).toEqual("£99.00");
   });
 
 }); 
