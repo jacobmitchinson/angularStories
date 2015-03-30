a4adace7e6eee1cfd5a8d6fab6501140df108721
Jacob Mitchinson <jmitchinson+github@gmail.com>
2015-03-30 15:53:16 +0100

    Updated homepage controller to pass test and total the price

--- app/js/homepageController.js
+++ app/js/homepageController.js
@@ -9,4 +9,15 @@ fashionApp.controller('homepageController', function($scope, $http) {
     $scope.basket.push(item);
   };
 
+  $scope.remove = function(item) { 
+    $scope.basket.pop(item);
+  };
+
+  $scope.total = function() { 
+    var total = 0;
+    for(var i = 0; i < $scope.basket.length; i++) { 
+      total += $scope.basket[i].price;
+    };
+    return total;
+  };
 });
