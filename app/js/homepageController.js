708cc66a754b3fb47f69aad85f3d4c2741993c2d
Jacob Mitchinson <jmitchinson+github@gmail.com>
2015-04-01 21:15:08 +0100

    Fixed bug that prevented adding more than one item

--- app/js/homepageController.js
+++ app/js/homepageController.js
@@ -13,18 +13,35 @@ fashionApp.controller('homepageController', function($scope, $http) {
   $scope.voucher;
   $scope.message = "No voucher applied.";
 
+  // if the item exists in the basket increase the item quantity in the basket
+
   $scope.add = function(item) { 
-    $scope.basket.push(item);
+    if($scope._hasItemInBasket(item)){
+      var index = $scope.basket.indexOf(item);
+      $scope.basket[index].desiredQuantity += 1;
+    } else { 
+      item.desiredQuantity = 1;
+      $scope.basket.push(item);
+    }
   };
 
   $scope.remove = function(item) { 
     $scope.basket.pop(item);
   };
 
+  $scope._hasItemInBasket = function(item) { 
+    for(var i = 0; $scope.basket.length; i++) { 
+      if($scope.basket[i] === item) { 
+        return true;
+      }; 
+    };
+    return false;
+  };
+
   $scope.calculateTotal = function() { 
     $scope.total = 0;
     for(var i = 0; i < $scope.basket.length; i++) { 
-      $scope.total += $scope.basket[i].price;
+      $scope.total += ($scope.basket[i].price * $scope.basket[i].desiredQuantity);
     };
     return $scope.total;
   };
@@ -39,6 +56,8 @@ fashionApp.controller('homepageController', function($scope, $http) {
       $scope.message = "Invalid voucher."
     }  
   };
+
+  // TODO: this needs refactoring
 
   $scope._check = function(voucher) { 
     for(var i = 0; i < $scope.vouchers.length; i++) {
@@ -62,6 +81,8 @@ fashionApp.controller('homepageController', function($scope, $http) {
       return true;
     }
   };
+
+  // TODO: this needs refactoring
 
   $scope._checkCategory = function(voucher) { 
     if(voucher.categoryConditions != undefined) { 
