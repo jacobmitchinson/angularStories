29bb125281e7a4eb30f59071bfffb2b4a5e287d7
Jacob Mitchinson <jmitchinson+github@gmail.com>
2015-04-01 23:30:19 +0100

    Updated tests to comply with forthcoming changes to view

--- test/feature/homepageFeature.js
+++ test/feature/homepageFeature.js
@@ -10,35 +10,39 @@ describe('Homepage', function() {
   });
 
   it('will hide unavailable items', function() { 
-    var items = element.all(by.repeater('item in clothes'));
-    items.getText().then(function(item) { 
-      expect(item[4]).toEqual('');
-    });
+    var isElDisabled = element.all(by.tagName('input')).get(4).getAttribute('disabled');
+    expect(isElDisabled).toEqual('true');
   });
 
   it('can display the names of the products', function() { 
     var item = element.all(by.binding('item.name')).first();
-    expect(item.getText()).toEqual('Almond Toe Court Shoes');
+    expect(item.getText()).toContain('Almond Toe Court Shoes');
   });
 
   it('can display the color of the items', function() { 
     var item = element.all(by.binding('item.colour')).first();
-    expect(item.getText()).toEqual('Patent Black');
+    expect(item.getText()).toContain('Patent Black');
   });
 
   it('can display the category of the items', function() { 
     var item = element.all(by.binding('item.category')).first();
-    expect(item.getText()).toEqual("Women's Footwear");
+    expect(item.getText()).toContain("Women's Footwear");
   });
 
   it('can display the available quantity', function() { 
     var item = element.all(by.binding('item.quantity')).first();
-    expect(item.getText()).toEqual('5');
+    expect(item.getText()).toContain('5');
   });
 
   it('can display the price of each item', function() { 
     var item = element.all(by.binding('item.price')).first();
-    expect(item.getText()).toEqual('£99.00');
+    expect(item.getText()).toContain('£99.00');
+  });
+
+  it('can display the image of the item', function() { 
+    var items = element.all(by.repeater('item in clothes')).first();
+    var src = items.element(by.tagName('img')).getAttribute('src');
+    expect(src).toEqual("http://localhost:8000/app/images/black_shoe.jpg");
   });
 
 }); 
