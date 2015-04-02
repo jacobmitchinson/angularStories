4ed57d94273fbca1a17f6bf0baa03f0d8a7ae405
Jacob Mitchinson <jmitchinson+github@gmail.com>
2015-04-02 11:03:50 +0100

    Updated app.js to use partials

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,18 @@
-var fashionApp = angular.module('fashionApp', []);
+var fashionApp = angular.module('fashionApp', [
+  'ngResource',
+  'ngRoute'
+]);
+
+fashionApp.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/shop', {
+        templateUrl: 'partials/shop.html'
+      }).
+      when('/checkout', {
+        templateUrl: 'partials/checkout.html'
+      }).
+      otherwise({
+        redirectTo: '/shop'
+      });
+  }]);
